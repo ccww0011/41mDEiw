87c1ad47c41757efb6bd94b04183e47cbf59e45e
skwo0016 <skwo0016@student.monash.edu>
2025-12-09 02:35:16 +1100

    Produced FX. Reduced num of api calls in stock and fx. Fixed loading mechanism.

---
+++ personal-app/src/context/FxContext.js
@@ -0,0 +1,38 @@
+'use client';
+
+import {createContext, useContext, useEffect, useState} from 'react';
+import {useTransactions} from "@/context/TransactionContext";
+import {getInitialFxs} from "@/hooks/useFxDatabase";
+
+const FxContext = createContext();
+
+export function useFxs() {
+  const context = useContext(FxContext);
+  if (!context) throw new Error("useFxs must be used within FxProvider");
+  return context;
+}
+
+export function FxProvider({ children }) {
+  const [fxs, setFxs] = useState({});
+  const [loadingFxs, setLoadingFxs] = useState(false);
+  const {currencies} = useTransactions();
+
+  useEffect(() => {
+    const date = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    async function fetchData() {
+      setLoadingFxs(true);
+      await getInitialFxs(currencies, date, setFxs);
+      setLoadingFxs(false);
+    }
+    fetchData();
+  }, [currencies]);
+
+  // console.log(currencies)
+  console.log(fxs)
+
+  return (
+    <FxContext.Provider value={{ fxs, setFxs, loadingFxs, setLoadingFxs }}>
+      {children}
+    </FxContext.Provider>
+  );
+}
